863662cea895238925122a9a7e84ee8de87854ba
SucharitaDhar <64468430+SucharitaDhar@users.noreply.github.com>
2020-06-14 20:34:25 +0530

    created inc folders to contain include files, parameterized links of employee name to fetch data from api

--- inc/employeedata.php
+++ inc/employeedata.php
@@ -1,6 +1,13 @@
 <?php
 
-$api_url = 'https://bio.torre.co/api/bios/johnnam93';
+
+
+$name = $_GET['name'];
+
+
+$api_url = "https://bio.torre.co/api/bios/".$name;
+
+
 
 // Read JSON file
 $json_data = file_get_contents($api_url);
@@ -9,20 +16,22 @@ $json_data = file_get_contents($api_url);
 $user_data = json_decode($json_data);
 
 // Cut long data into small & select only first 10 records
-$user_data = array_slice($user_data, 0, 9);
+//$user_data = array_slice($user_data, 0, 9);
 
 // Print data if need to debug
 //print_r($user_data);
 
 // Traverse array and display user data
 foreach ($user_data as $user) {
-	echo "name: ".$user->employee_name;
+	echo "name: ".$user->name;
 	echo "<br />";
-	echo "name: ".$user->employee_age;
+	echo "name: ".$user->professionalHeadline;
 	echo "<br /> <br />";
+
 }
 
 ?>
+
 
 
 
--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 							<img src="https://starrgate.s3.amazonaws.com:443/users/e23281ba605e57e3dedb1d547294c6fec2efd2b0/profile_cdWKzNe.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$25+/hourly</h4>
-								<h4><a href='inc/employeedata.php'>John Nam</a>
+								<h4><a href='inc/employeedata.php?name=johnnam93'>John Nam</a>
 								</h4>
 								<p>Former Business Franchise Partner</p>
 							</div>
@@ -52,7 +52,7 @@
 							<img src="https://starrgate.s3.amazonaws.com:443/users/935e76f4ccbb149be3867a49539b12a799093c32/profile_g0gSnhq.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$800+/monthly</h4>
-								<h4><a href='inc/employeedata.php'>Henry Belmonte</a>
+								<h4><a href='inc/employeedata.php?name=henrybaguilar'>Henry Belmonte</a>
 								</h4>
 								<p>Front End Developer, SEO/SEM Analyst</p>
 							</div>
@@ -76,7 +76,7 @@
 							<img src="https://starrgate.s3.amazonaws.com:443/users/4c88143419c7212f74a29d9efb3d068aa7a95905/profile_iRN2kD2.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$70+/hourly</h4>
-								<h4><a href='inc/employeedata.php'>Nicolaz David Perez Mesa</a>
+								<h4><a href='inc/employeedata.php?name=nicolazdavidperezmesa'>Nicolaz David Perez Mesa</a>
 								</h4>
 								<p>Software Engineer and Education Lead</p>
 							</div>
@@ -98,7 +98,7 @@
 							<img src="https://starrgate.s3.amazonaws.com:443/users/131a8c1aa78105149b539af508da745360345cc9/profile_nj3NWoE.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$60+/hourly</h4>
-								<h4><a href='employeedata.php'>Jorge Andres</a>
+								<h4><a href='inc/employeedata.php?name=joranboc'>Jorge Andres</a>
 								</h4>
 								<p>Systems Engineer</p>
 							</div>
@@ -120,7 +120,7 @@
 							<img src="https://starrgate.s3.amazonaws.com:443/users/58d23b813023eff954c24f015e96c8612d2b4334/profile_ootrR4d.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$4k+/monthly</h4>
-								<h4><a href='employeedata.php'>David Jara</a>
+								<h4><a href='inc/employeedata.php?name=davidandresjara'>David Jara</a>
 								</h4>
 								<p>Software Developer</p>
 							</div>
