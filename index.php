eae39513da5b6d3c663cbd4d0898be0d017895c9
SucharitaDhar <64468430+SucharitaDhar@users.noreply.github.com>
2020-06-14 17:41:13 +0530

    Added images for employees and made a php file employeedata.php to fetch employee data from the endpoints

--- index.php
+++ index.php
@@ -31,15 +31,15 @@
 				<div class='row'>
 				 <div class='col-sm-4 col-lg-4 col-md-4'>
 						<div class='thumbnail'>
-							<img src='http://placehold.it/320x150' alt=''>
+							<img src="https://starrgate.s3.amazonaws.com:443/users/e23281ba605e57e3dedb1d547294c6fec2efd2b0/profile_cdWKzNe.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$25+/hourly</h4>
-								<h4><a href='inc/productimages.php'>John Nam</a>
+								<h4><a href='inc/employeedata.php'>John Nam</a>
 								</h4>
 								<p>Former Business Franchise Partner</p>
 							</div>
 							<div class='ratings'>
-								<p class='pull-right'>16 reviews</p>
+								<p class='pull-right'>15 reviews</p>
 								<p>
 									<span class='glyphicon glyphicon-star'></span>
 									<span class='glyphicon glyphicon-star'></span>
@@ -53,10 +53,10 @@
 
 					<div class='col-sm-4 col-lg-4 col-md-4'>
 						<div class='thumbnail'>
-							<img src='http://placehold.it/320x150' alt=''>
+							<img src="https://starrgate.s3.amazonaws.com:443/users/935e76f4ccbb149be3867a49539b12a799093c32/profile_g0gSnhq.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$800+/monthly</h4>
-								<h4><a href='inc/productimages.php'>Henry Belmonte128</a>
+								<h4><a href='inc/employeedata.php'>Henry Belmonte</a>
 								</h4>
 								<p>Front End Developer, SEO/SEM Analyst</p>
 							</div>
@@ -77,10 +77,10 @@
 						<div class='thumbnail'>
 
 
-							<img src='http://placehold.it/320x150' alt=''>
+							<img src="https://starrgate.s3.amazonaws.com:443/users/4c88143419c7212f74a29d9efb3d068aa7a95905/profile_iRN2kD2.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$70+/hourly</h4>
-								<h4><a href='inc/productimages.php'>Nicolaz David Perez Mesa</a>
+								<h4><a href='inc/employeedata.php'>Nicolaz David Perez Mesa</a>
 								</h4>
 								<p>Software Engineer and Education Lead</p>
 							</div>
@@ -99,10 +99,10 @@
 
 					<div class='col-sm-4 col-lg-4 col-md-4'>
 						<div class='thumbnail'>
-							<img src='http://placehold.it/320x150' alt=''>
+							<img src="https://starrgate.s3.amazonaws.com:443/users/131a8c1aa78105149b539af508da745360345cc9/profile_nj3NWoE.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$60+/hourly</h4>
-								<h4><a href='inc/productimages.php'>Jorge Andres</a>
+								<h4><a href='employeedata.php'>Jorge Andres</a>
 								</h4>
 								<p>Systems Engineer</p>
 							</div>
@@ -121,10 +121,10 @@
 
 					<div class='col-sm-4 col-lg-4 col-md-4'>
 						<div class='thumbnail'>
-							<img src='http://placehold.it/320x150' alt=''>
+							<img src="https://starrgate.s3.amazonaws.com:443/users/58d23b813023eff954c24f015e96c8612d2b4334/profile_ootrR4d.jpg" alt=''>
 							<div class='caption'>
 								<h4 class='pull-right'>$4k+/monthly</h4>
-								<h4><a href='inc/productimages.php'>David Jara</a>
+								<h4><a href='employeedata.php'>David Jara</a>
 								</h4>
 								<p>Software Developer</p>
 							</div>
